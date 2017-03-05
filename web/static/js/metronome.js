20a1284810e574fc949b779cbff28f052da7dfd0
Emanuel Evans <mail@emanuel.industries>
2017-03-04 18:55:06 -0800

    Switch to Tock for supposed accuracy
    Still doesn't sound great...

--- web/static/js/metronome.js
+++ web/static/js/metronome.js
@@ -1,13 +1,15 @@
+import Tock from 'tocktimer';
+
 export default () => {
   const metroElem = document.getElementById('metronome');
   const playerElem = document.getElementById('player');
   const tempoElem = document.getElementById('tempo');
   const audio = new window.Audio('images/metro.mp3');
+  let timer = null;
 
-  let player = null;
   const playBeat = () => {
     audio.play();
-  }
+  };
 
   const start = () => {
     const tempo = parseInt(tempoElem.value);
@@ -15,20 +17,24 @@ export default () => {
       alert("No tempo!");
       return;
     }
-    playBeat();
     const interval = 60000 / tempo;
-    player = window.setInterval(playBeat, interval);
+    timer = new Tock({
+      interval,
+      callback: playBeat,
+    });
+    timer.start();
     playerElem.textContent = 'PLAYING';
   };
 
   const stop = () => {
     window.clearInterval(player);
-    player = null;
+    timer.stop();
+    timer = null;
     playerElem.textContent = '';
   };
 
   metroElem.onclick = () => {
-    if (player) {
+    if (timer) {
       stop();
     } else {
       start();
